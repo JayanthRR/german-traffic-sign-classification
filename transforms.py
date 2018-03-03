50af7e153318f9d59e52e16eda8ada68c8a2efff
JayanthRR <htnayajregatti@gmail.com>
2018-03-03 06:00:54 -0500

    added adaptive transformations based on the model used

---
+++ transforms.py
@@ -0,0 +1,60 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+from skimage import io, transform
+import os
+import torch
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import torchvision
+
+
+
+# In[3]:
+
+
+vgg_train_transform = transforms.Compose([
+    transforms.RandomSizedCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+resnet_train_transform = transforms.Compose([
+    transforms.RandomSizedCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+inception_train_transform = transforms.Compose([
+    transforms.RandomSizedCrop(299),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+
+# In[4]:
+
+
+vgg_test_transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+resnet_test_transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+inception_test_transform = transforms.Compose([
+    transforms.Resize((299,299)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
